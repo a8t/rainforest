6a5942f97e27ed05903a5b1fcf0a491220441165
Andy Tran <andy8tran@gmail.com>
2017-09-07 14:31:35 -0400

    finished CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,28 +4,55 @@ class ProductsController < ApplicationController
 
   end
 
-  def create
-
-  end
-
   def new
-
+    @product = Product.new
   end
 
-  def edit
+  def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
 
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+    product = @product
+  end
 
+  def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
 
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
 
+  def product_params
+    params.require(:product).permit(:name, :description,:price_in_cents)
   end
 
 end
